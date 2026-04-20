642a1227fb8a1376da5defde0080bbb27c0201d7
Vignesh K <144013983+vickydecodes@users.noreply.github.com>
2026-04-20 13:40:58 +0530

    fix: fixed the api format

--- frontend/src/api/history.js
+++ frontend/src/api/history.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const API = import.meta.env.VITE_API_URL ?? "http://127.0.0.1:8000/api";
+
 export const fetchHistory = async () => {
-  const res = await axios.get("http://127.0.0.1:8000/api/history");
+  const res = await axios.get(`${API}/history`);
   return res.data.history;
 };
